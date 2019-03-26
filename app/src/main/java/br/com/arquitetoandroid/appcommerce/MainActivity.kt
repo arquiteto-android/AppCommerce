859a138fffe3d6894d3a6d58351fb09a572017b1
Fillipe Cordeiro <fillipe@androidpro.com.br>
2019-03-26 15:11:25 -0300

    Etapa 4

--- app/src/main/java/br/com/arquitetoandroid/appcommerce/MainActivity.kt
+++ app/src/main/java/br/com/arquitetoandroid/appcommerce/MainActivity.kt
@@ -1,12 +1,20 @@
 package br.com.arquitetoandroid.appcommerce
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val productItem: LinearLayout = findViewById(R.id.ll_product_item)
+        productItem.setOnClickListener{
+            val intent: Intent = Intent(this, ProductDetailActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/br/com/arquitetoandroid/appcommerce/ProductDetailActivity.kt
+++ app/src/main/java/br/com/arquitetoandroid/appcommerce/ProductDetailActivity.kt
@@ -8,5 +8,11 @@ class ProductDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_detail)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
