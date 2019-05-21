7dcc3b0a29a68083cec6f48f4130303450f2c0a4
Fillipe Cordeiro <fillipe@androidpro.com.br>
2019-05-21 15:58:27 -0300

    Etapa 8

--- app/src/main/java/br/com/arquitetoandroid/appcommerce/MainActivity.kt
+++ app/src/main/java/br/com/arquitetoandroid/appcommerce/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
@@ -17,6 +18,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     lateinit var toolbar: Toolbar
     lateinit var drawerLayout: DrawerLayout
     lateinit var navigationView: NavigationView
+    lateinit var textTitle: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +26,11 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        textTitle = findViewById(R.id.toolbar_title)
+        textTitle.text = getString(R.string.app_name)
 
         drawerLayout = findViewById(R.id.nav_drawer_layout)
 
--- app/src/main/java/br/com/arquitetoandroid/appcommerce/ProductDetailActivity.kt
+++ app/src/main/java/br/com/arquitetoandroid/appcommerce/ProductDetailActivity.kt
@@ -1,14 +1,27 @@
 package br.com.arquitetoandroid.appcommerce
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class ProductDetailActivity : AppCompatActivity() {
+
+    lateinit var toolbar: Toolbar
+    lateinit var textTitle: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_detail)
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        textTitle = findViewById(R.id.toolbar_title)
+        textTitle.text = "Titulo do Produto"
     }
 
     override fun onSupportNavigateUp(): Boolean {
